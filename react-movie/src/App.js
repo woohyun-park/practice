290560cb2b64e172d978afbd178d389b1beb09b4
Woohyun Park <woohyunpark.mail@gmail.com>
2021-04-26 00:54:37 +0900

    react-movie: LEARN, PropTypes, Component

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1}));
-  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false});
+  }
+  componentDidMount(){
+    this.getMovies();
+  }
   render(){
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <div>{isLoading ? "Loading" : movies.map(movie => {
+        return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.poster} />
+      })}</div>
     );
   };
 }
