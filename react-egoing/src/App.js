fd1d0f345fbb0a0e6ed8e25433a2dc01d2619b72
Woohyun Park <woohyunpark.mail@gmail.com>
2021-05-01 00:51:18 +0900

    react-egoing: LEARN, learn render, preventDefault, setState, bind

--- react-egoing/src/App.js
+++ react-egoing/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode: 'welcome',
       subject: {title: 'WEB', sub: 'World Wide Web'},
+      welcome: {title:'Welcom', desc:'Hello, React!'},
       contents: [
         {id:1, title:'HTML', desc:'HTML is HyperText ...'},
         {id:2, title:'CSS', desc:'CSS is for design ...'},
@@ -17,6 +19,15 @@ class App extends Component {
     }
   }
   render() {
+    let _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject
@@ -24,7 +35,7 @@ class App extends Component {
           sub={this.state.subject.sub}>
         </Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
