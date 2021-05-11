be7d62f15a6a96423feebff3494e1dd788560b13
Woohyun Park <woohyunpark.mail@gmail.com>
2021-05-11 10:59:47 +0900

    angular-tour-of-heroes: ADD, add 5. navigation with routing

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,17 +10,11 @@ import { MessageService } from "../message.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
