80a0a262215f437db265b72ab9a413e197d4376f
Woohyun Park <woohyunpark.mail@gmail.com>
2021-05-16 21:54:30 +0900

    refactoring: REFACTOR, refactor by factoring out playFor()

--- refactoring/Chapter1/test.js
+++ refactoring/Chapter1/test.js
@@ -72,29 +72,31 @@ const resultOrigin = statementOrigin(invoices, plays);
 
 
 
+function playFor(aPerformance){
+  return plays[aPerformance.playID];
+}
 
+function amountFor(aPerformance){
+  let result = 0;
 
-function amountFor(perf, play){
-  let thisAmount = 0;
-
-  switch (play.type){
+  switch (playFor(aPerformance).type){
     case "tragedy":
-      thisAmount = 40000;
-      if(perf.audience > 30){
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if(aPerformance.audience > 30){
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
-      if(perf.audience > 20){
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if(aPerformance.audience > 20){
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Erorr(`알 수 없는 장르: ${play.type}`);
+      throw new Erorr(`알 수 없는 장르: ${playFor(aPerformance).type}`);
   }
-  return thisAmount;
+  return result;
 }
 
 function statement(invoice, plays){
@@ -104,16 +106,11 @@ function statement(invoice, plays){
   const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USE", minimumFractionDigits: 2}).format;
 
   for(let perf of invoice.performances){
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    thisAmount = amountFor(perf, play);
-
     volumeCredits += Math.max(perf.audience - 30, 0);
-    if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `총액: ${format(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
