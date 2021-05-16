88620433553b48758e831d0285a64b7840711055
Woohyun Park <woohyunpark.mail@gmail.com>
2021-05-16 22:06:02 +0900

    refactoring: REFACTOR, refactor by factoring out totalVolumeCredits()

--- refactoring/Chapter1/test.js
+++ refactoring/Chapter1/test.js
@@ -68,7 +68,13 @@ const resultOrigin = statementOrigin(invoices, plays);
 
 
 
-
+function totalVolumeCredits(invoice){
+  let volumeCredits = 0;
+  for(let perf of invoice.performances){
+    volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits;
+}
 
 function usd(aNumber){
   return new Intl.NumberFormat("en-US", { style: "currency", currency: "USE", minimumFractionDigits: 2}).format(aNumber/100);
@@ -111,15 +117,13 @@ function amountFor(aPerformance){
 
 function statement(invoice, plays){
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for(let perf of invoice.performances){
-    volumeCredits += volumeCreditsFor(perf);
-
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
+  let volumeCredits = totalVolumeCredits(invoice);
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
