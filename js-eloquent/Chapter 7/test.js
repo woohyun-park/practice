95aee3f614d8215e2ad6ce1e62e59771f0fa4eee
Woohyun Park <woohyunpark.mail@gmail.com>
2021-05-25 09:07:21 +0900

    ADD, add C7 - move

--- js-eloquent/Chapter 7/test.js
+++ js-eloquent/Chapter 7/test.js
@@ -24,5 +24,33 @@ function buildGraph(edge){
   return graph;
 }
 
-const graph = buildGraph(roads);
-console.log(graph);
+const roadGraph = buildGraph(roads);
+
+class VillageState{
+  constructor(place, parcels){
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination){
+    if(!roadGraph[this.place].includes(destination)){
+      return this;
+    } else {
+      //map은 물건의 이동, filter는 물건의 배달을 처리
+      let parcels = this.parcels.map(p => {
+        if(p.place != this.place) return p;
+        return { place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+let first = new VillageState(
+  "Post Office", [{place: "Post Office", address: "Alice's House"}]
+)
+let next = first.move("Alice's House");
+
+console.log(next.place);
+console.log(next.parcels);
+console.log(first.place);
