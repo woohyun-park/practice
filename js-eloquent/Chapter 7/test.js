a70cf28bd57adeb6f55a6738aacefdd88614e887
Woohyun Park <woohyunpark.mail@gmail.com>
2021-05-25 10:17:38 +0900

    ADDm add C7 - routeRobot

--- js-eloquent/Chapter 7/test.js
+++ js-eloquent/Chapter 7/test.js
@@ -54,3 +54,54 @@ let next = first.move("Alice's House");
 console.log(next.place);
 console.log(next.parcels);
 console.log(first.place);
+
+function runRobot(state, robot, memory){
+  for (let turn = 0;;turn++){
+    if (state.parcels.length == 0){
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+function randomPick(array){
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state){
+  return { direction: randomPick(roadGraph[state.place])};
+}
+
+const mailRoute = [
+  "Alice's House", "Cabin", "Alice's House", "Bob's House",
+  "Town Hall", "Daria's House", "Ernie's House",
+  "Grete's House", "Shop", "Grete's House", "Farm",
+  "Marketplace", "Post Office"
+]
+
+function routeRobot(state, memory){
+  if (memory.length == 0){
+    memory = mailRoute;
+  }
+  return { direction: memory[0], memory: memory.slice(1)};
+}
+
+VillageState.random = function(parcelCount = 5){
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++){
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while(place == address);
+    parcels.push({place, address});
+  }
+  return new VillageState("Post Office", parcels);
+}
+
+runRobot(VillageState.random(), routeRobot, mailRoute);
