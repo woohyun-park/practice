5672839dc05f5a5082e7a8d23a7952c22b2e0523
Woohyun Park <woohyunpark.mail@gmail.com>
2021-06-06 18:17:41 +0900

    js-eloquent: ADD, add C8 '잠긴 상자'

--- js-eloquent/Chapter 8/test.js
+++ js-eloquent/Chapter 8/test.js
@@ -1,3 +1,4 @@
+//재시도
 class MultiplicatorUnitFailure extends Error {}
 
 function primitiveMultiply(n1, n2){
@@ -17,3 +18,46 @@ for(;;){
     console.log(e);
   }
 }
+
+//잠긴 상자
+const box = {
+  locked: true,
+  unlock(){
+    this.locked = false;
+  },
+  lock(){
+    this.locked = true;
+  },
+  _content: [],
+  get content(){
+    if(this.locked){
+      throw new Error("Locked!");
+    }
+    return this._content;
+  }
+}
+
+function withBoxUnlocked(body){
+  const locked = box.locked;
+  box.unlock();
+  try{
+    body();
+  } finally{
+    if(locked){
+      box.lock();
+    }
+  }
+}
+
+withBoxUnlocked(function(){
+  box.content.push("gold piece");
+});
+
+try{
+  withBoxUnlocked(function(){
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch(e){
+  console.log("Error raised: ", e);
+}
+console.log(box.locked);
