67da1a381a1b5c6ba4265a11573743ae015a5eb0
Woohyun Park <woohyunpark.mail@gmail.com>
2021-06-06 14:42:58 +0900

    canvas-test: PRACTICE, practice 안티패턴

---
+++ canvas-test/index.js
@@ -0,0 +1,7 @@
+const body = document.getElementsByTagName("body")[0];
+body.style.color = "Blue";
+body.style.fontFamily = "Time New Roman";
+
+const { color, fontFamily } = body.style;
+
+console.log(color, fontFamily);
