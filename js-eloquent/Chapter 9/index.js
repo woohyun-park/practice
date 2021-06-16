5dfb6f10c25eb617e6fb1826c236d147c040ccd9
Woohyun Park <woohyunpark.mail@gmail.com>
2021-06-16 23:19:11 +0900

    js-eloquent: ADD, add C9 숫자 반복

--- js-eloquent/Chapter 9/index.js
+++ js-eloquent/Chapter 9/index.js
@@ -29,3 +29,22 @@ console.log("this contains CONTAINER container".match(regex7));
 const regex8 = /\W'|'\W/g;
 // console.log(" 'This isn't real' ".match(regex8));
 console.log(" 'This isn't real' ".replace(regex8, "\""));
+
+//3. 숫자 반복
+// (\+|-)?: +-가 있거나 없거나
+// (\d+(\.\d*)?: 숫자, 숫자.숫자의 형태이거나
+// (\.\d+): .숫자의 형태이거나
+// (((e|E)(\+|-)?)\d+)?: e숫자 또는 E숫자의 형태이거나
+const number = /^(\+|-)?((\d+(\.\d*)?)|(\.\d+))(((e|E)(\+|-)?)\d+)?$/;
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
